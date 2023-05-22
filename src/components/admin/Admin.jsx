1ba87341ba56ddfa589efaa6eeb3115b64ab722b
jespersjostrom <jespersjostrom2@gmail.com>
2023-05-22 21:53:06 +0300

    feat: admin page

---
+++ src/components/admin/Admin.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import './admin.css'
+
+const Admin = () => {
+  const location = useLocation()
+
+  return (
+    <div className="loginpage-container">
+    <div className='box'>
+      <div className="content">
+      <h1>Admin Panel</h1>
+      <h4> User {location.state.id} is now logged in </h4>
+      <p> Admin Page WIP </p>
+      <a href="/" className="btn-cta">Log Out</a>
+      </div>
+            <i></i>
+        </div>
+        </div>
+  )
+}
+
+export default Admin
