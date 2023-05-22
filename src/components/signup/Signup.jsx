2b40873d21063926fd1c1abaf79a1a58b294458f
jespersjostrom <jespersjostrom2@gmail.com>
2023-05-23 02:01:59 +0300

    fix: imports

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import axios from "axios"
-import { useNavigate, Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import './signup.css'
 
 function Signup() {
