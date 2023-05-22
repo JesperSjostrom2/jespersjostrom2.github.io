2956143b56b03cca3a298a31e87ea84cbb58b30f
jespersjostrom <jespersjostrom2@gmail.com>
2023-05-22 16:05:20 +0300

    feat: login router & styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react'
-import Navbar from './components/navbar/Navbar'
-import Intro from './components/intro/Intro'
-import About from './components/about/About'
-import Education from './components/education/Education'
-import Skills from './components/skills/Skills'
-import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
-
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Intro from './components/intro/Intro';
+import About from './components/about/About';
+import Education from './components/education/Education';
+import Skills from './components/skills/Skills';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+import Loginpage from './components/loginpage/Loginpage';
 
 function App() {
   return (
-    <>
-    <Navbar/>
-    <Intro/>
-    <About/>
-    <Education/>
-    <Skills/>
-    <Contact/>
-    <Footer/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<>
+          <Navbar />
+          <Intro />
+          <About />
+          <Education />
+          <Skills />
+          <Contact />
+          <Footer />
+        </>} />
+        <Route path="/login" element={<Loginpage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/loginpage/Loginpage.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './loginpage.css'
+
+const Loginpage = () => {
+  return (
+    <div className="loginpage-container">
+    <div className='box'>
+      <form>
+        <h2> Sign in </h2>
+        <div className="inputBox">
+          <input type="text" required="required"/>
+            <span>Username</span>
+            <i></i>
+        </div>
+        <div className="inputBox">
+          <input type="password" required="required"/>
+            <span>Password</span>
+            <i></i>
+        </div>
+        <div className="links">
+          <a href="#">Forgot Password</a>
+          <a href="#">Signup</a>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+    </div>
+  )
+}
+
+export default Loginpage
