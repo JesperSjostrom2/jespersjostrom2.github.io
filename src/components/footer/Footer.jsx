d07e47548b1cbb652228c2b8b4ab93378d094f79
jespersjostrom <jespersjostrom2@gmail.com>
2023-05-23 03:03:16 +0300

    updated footer date

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer class="footer">
       <div className="footer-text">
-        <p>Jesper Sjöström - Last updated 2023-05-18 </p>
+        <p>Jesper Sjöström - Last updated 2023-05-23 </p>
       </div>
 
       <div className="footer-iconTop">
