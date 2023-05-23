85670386103513ef081446cd70eabc24c655b13d
jespersjostrom <jespersjostrom2@gmail.com>
2023-05-23 03:03:07 +0300

    style: styling fix

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -34,77 +34,77 @@ const Skills = () => {
       <h1 className="skills-header" data-aos="fade-up" data-aos-delay="400"> My <span>Skills</span></h1>
       <h3 className="skills-technologies" data-aos="fade-up" data-aos-delay="500">Technologies that I am familiar with</h3>
 
-      <div className="skills-icons">
+      <div className="skills-icons" data-aos="fade-up" data-aos-delay="600">
 
-      <div className="box-box" data-aos="fade-right" data-aos-delay="600"><DiJavascript1 style={style}/>
+      <div className="box-box"><DiJavascript1 style={style}/>
       <h3>Javascript</h3>
       </div>
 
-      <div className="box-box" data-aos="fade-right" data-aos-delay="700"><AiFillHtml5 style={style}/>
+      <div className="box-box"><AiFillHtml5 style={style}/>
       <h3>HTML5</h3>
       </div>
 
-      <div className="box-box"data-aos="fade-right" data-aos-delay="800"><IoLogoCss3 style={style}/>
+      <div className="box-box"><IoLogoCss3 style={style}/>
       <h3>CSS3</h3>
       </div>
 
-      <div className="box-box"data-aos="fade-right" data-aos-delay="900"><FaReact style={style}/>
+      <div className="box-box"><FaReact style={style}/>
       <h3>React</h3>
       </div>
 
-      <div className="box-box"data-aos="fade-right" data-aos-delay="1000"><FaNodeJs style={style}/>
+      <div className="box-box"><FaNodeJs style={style}/>
       <h3>Node</h3>
       </div>
 
-      <div className="box-box"data-aos="fade-right" data-aos-delay="1100"><BsGithub style={style}/>
+      <div className="box-box"><BsGithub style={style}/>
       <h3>GitHub</h3>
       </div>
 
-      <div className="box-box"data-aos="fade-right" data-aos-delay="1200"><BsGit style={style}/>
+      <div className="box-box"><BsGit style={style}/>
       <h3>Git</h3>
       </div>
 
-      <div className="box-box"data-aos="fade-right" data-aos-delay="1300"><FaBootstrap style={style}/>
+      <div className="box-box"><FaBootstrap style={style}/>
       <h3>Bootstrap</h3>
       </div>
 
-      <div className="box-box"data-aos="fade-right" data-aos-delay="1400"><SiMicrosoftazure style={style}/>
+      <div className="box-box"><SiMicrosoftazure style={style}/>
       <h3>Azure</h3>
       </div>
 
-      <div className="box-box"data-aos="fade-right" data-aos-delay="1500"><DiMongodb style={style}/>
+      <div className="box-box"><DiMongodb style={style}/>
       <h3>MongoDB</h3>
       </div>
 
-      <div className="box-box"data-aos="fade-right" data-aos-delay="1600"><FaAws style={style}/>
+      <div className="box-box"><FaAws style={style}/>
       <h3>AWS</h3>
       </div>
 
-      <div className="box-box"data-aos="fade-right" data-aos-delay="1700"><DiHeroku style={style}/>
+      <div className="box-box"><DiHeroku style={style}/>
       <h3>Heroku</h3>
       </div>
 
-      <div className="box-box"data-aos="fade-right" data-aos-delay="1800"><TbBrandCypress style={style}/>
+      <div className="box-box"><TbBrandCypress style={style}/>
       <h3>Cypress</h3>
       </div>
 
-      <div className="box-box"data-aos="fade-right" data-aos-delay="1900"><SiPostman style={style}/>
+      <div className="box-box"><SiPostman style={style}/>
       <h3>Postman</h3>
       </div>
 
-      <div className="box-box"data-aos="fade-right" data-aos-delay="2000"><TbBrandBackbone style={style}/>
+      <div className="box-box"><TbBrandBackbone style={style}/>
       <h3>Backbone</h3>
       </div>
 
-      <div className="box-box"data-aos="fade-right" data-aos-delay="2100"><FiFigma style={style}/>
+      <div className="box-box"><FiFigma style={style}/>
       <h3>Figma</h3>
       </div>
 
-      <div className="box-box"data-aos="fade-right" data-aos-delay="2200"><FaLess style={style}/>
+      <div className="box-box"><FaLess style={style}/>
       <h3>Less</h3>
       </div>
 
-      <div className="box-box"data-aos="fade-right" data-aos-delay="2300"><FaSass style={style}/>
+      <div className="box-box"><FaSass style={style}/>
       <h3>Sass</h3>
       </div>
       </div>
