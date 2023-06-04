66f4fa714fdf3a62e78a9cd64daa12bd39dc52b2
jespersjostrom <jespersjostrom2@gmail.com>
2023-06-04 15:36:06 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,3 +1,28 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Intro from './components/intro/Intro';
+import About from './components/about/About';
+import Education from './components/education/Education';
+import Skills from './components/skills/Skills';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+import Loginpage from './components/loginpage/Loginpage';
+import Adminpage from './components/admin/Admin';
+import Signup from './components/signup/Signup';
+import NotFound from './notfound/NotFound';
+import { Helmet } from 'react-helmet';
+
+function MainLayout(props) {
+  return (
+    <>
+      <Navbar />
+      {props.children}
+      <Footer />
+    </>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
