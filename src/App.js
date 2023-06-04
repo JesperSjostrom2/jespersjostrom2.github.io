fcbf655bb6e0c780123e7b057b1b21317d4e735f
jespersjostrom <jespersjostrom2@gmail.com>
2023-06-04 16:02:40 +0300

    test 2

--- src/App.js
+++ src/App.js
@@ -12,42 +12,25 @@ import Adminpage from './components/admin/Admin';
 import Signup from './components/signup/Signup';
 import { Helmet } from 'react-helmet';
 
-function MainLayout(props) {
-  return (
-    <>
-      <Navbar />
-      {props.children}
-      <Footer />
-    </>
-  );
-}
-
 function App() {
   return (
     <BrowserRouter>
+      <Helmet>
+        <title>Jesper Sjöström | Frontend Developer</title>
+        <meta name="description" content="I am a frontend developer who loves design and creating websites." />
+      </Helmet>
+      <Navbar />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-             <Helmet>
-                <title>Jesper Sjöström | Frontend Developer</title>
-                <meta name="description" content="I am a frontend developer who loves design and creating websites." />
-              </Helmet>
-              <MainLayout>
-                <Intro />
-                <About />
-                <Education />
-                <Skills />
-                <Contact />
-              </MainLayout>
-            </>
-          }
-        />
+        <Route path="/" element={<Intro />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Loginpage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/admin" element={<Adminpage />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
