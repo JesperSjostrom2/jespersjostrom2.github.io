f16e820cae830d3aedc3ab95f2d9857dc9ab6bc1
jespersjostrom <jespersjostrom2@gmail.com>
2023-06-04 20:13:48 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Intro from './components/intro/Intro';
@@ -12,20 +11,6 @@ import Adminpage from './components/admin/Admin';
 import Signup from './components/signup/Signup';
 import { Helmet } from 'react-helmet';
 
-function MainLayout(props) {
-  return (
-    <>
-      <Navbar />
-      <Intro />
-      <About />
-      <Education />
-      <Skills />
-      <Contact />
-      <Footer />
-    </>
-  );
-}
-
 function App() {
   return (
     <BrowserRouter>
@@ -33,13 +18,14 @@ function App() {
         <title>Jesper Sjöström | Frontend Developer</title>
         <meta name="description" content="I am a frontend developer who loves design and creating websites." />
       </Helmet>
+      <Navbar />
+      <Intro />
+      <About />
+      <Education />
+      <Skills />
+      <Contact />
+      <Footer />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <MainLayout />
-          }
-        />
         <Route path="/login" element={<Loginpage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/admin" element={<Adminpage />} />
