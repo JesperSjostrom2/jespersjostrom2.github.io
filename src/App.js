eceebbbac560bb9888d59f0800a43d5d1ac807a6
jespersjostrom <jespersjostrom2@gmail.com>
2023-06-04 16:37:11 +0300

    test push

--- src/App.js
+++ src/App.js
@@ -18,18 +18,14 @@ function App() {
         <title>Jesper Sjöström | Frontend Developer</title>
         <meta name="description" content="I am a frontend developer who loves design and creating websites." />
       </Helmet>
+      <Navbar />
+      <Intro />
+      <About />
+      <Education />
+      <Skills />
+      <Contact />
+      <Footer />
       <Routes>
-        <Route path="/" element={
-          <>
-            <Navbar />
-            <Intro />
-            <About />
-            <Education />
-            <Skills />
-            <Contact />
-            <Footer />
-          </>
-        } />
         <Route path="/login" element={<Loginpage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/admin" element={<Adminpage />} />
