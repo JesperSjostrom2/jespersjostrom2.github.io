0b02c144623f09baafcba9e255f106aa569c839c
jespersjostrom <jespersjostrom2@gmail.com>
2023-06-04 16:59:51 +0300

    revert

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Intro from './components/intro/Intro';
@@ -19,18 +18,14 @@ function App() {
         <title>Jesper Sjöström | Frontend Developer</title>
         <meta name="description" content="I am a frontend developer who loves design and creating websites." />
       </Helmet>
+      <Navbar />
+      <Intro />
+      <About />
+      <Education />
+      <Skills />
+      <Contact />
+      <Footer />
       <Routes>
-        <Route path="/" element={
-          <>
-            <Navbar />
-            <Intro />
-            <About />
-            <Education />
-            <Skills />
-            <Contact />
-            <Footer />
-          </>
-        } />
         <Route path="/login" element={<Loginpage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/admin" element={<Adminpage />} />
