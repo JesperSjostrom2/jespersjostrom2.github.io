03fce5a53749c958105141ce9c4af66d08350ca0
jespersjostrom <jespersjostrom2@gmail.com>
2024-06-09 16:54:38 +0300

    sticky navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,11 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './navbar.css';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import logo from '../../assets/logo.png';
 
 const Navbar = () => {
   const navRef = useRef();
+  const headerRef = useRef();
   const [isOpen, setIsOpen] = useState(false);
 
   const showNavbar = () => {
@@ -15,8 +16,24 @@ const Navbar = () => {
     setIsOpen(false);
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > headerRef.current.offsetTop) {
+        headerRef.current.classList.add('sticky');
+      } else {
+        headerRef.current.classList.remove('sticky');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="header">
+    <header ref={headerRef} className="header">
       <a href="#home">
         <img
           src={logo}
@@ -25,22 +42,14 @@ const Navbar = () => {
         />
       </a>
       <nav ref={navRef} className={`navbar ${isOpen ? 'responsive_nav' : ''}`}>
-        <a href="#about" onClick={closeNavbar}>
-          About
-        </a>
-        <a href="#education" onClick={closeNavbar}>
-          Education
-        </a>
-        <a href="#skills" onClick={closeNavbar}>
-          Skills
-        </a>
-        <a href="#contact" onClick={closeNavbar}>
-          Contact
-        </a>
+        <a href="#about" onClick={closeNavbar}>About</a>
+        <a href="#education" onClick={closeNavbar}>Education</a>
+        <a href="#skills" onClick={closeNavbar}>Skills</a>
+        <a href="#contact" onClick={closeNavbar}>Contact</a>
         <button className="cv">
-        <a href="https://github.com/jespersjostrom2/my-cv/raw/main/cv.pdf" download="cv.pdf">
-          <span>Download CV</span>
-          <i></i>
+          <a href="https://github.com/jespersjostrom2/my-cv/raw/main/cv.pdf" download="cv.pdf">
+            <span>Download CV</span>
+            <i></i>
           </a>
         </button>
         <button className="nav-btn nav-close-btn" onClick={showNavbar}>
