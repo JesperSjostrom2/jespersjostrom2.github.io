79e4611f4844bfd4a194b77b506af10467bd797a
jespersjostrom <jespersjostrom2@gmail.com>
2024-06-19 00:29:56 +0300

    corrected layout & hover effects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,6 +3,7 @@ import './projects.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import kermapng from '../../assets/kermaprojects.png';
+import { FaArrowRight } from "react-icons/fa";
 
 const Projects = () => {
   useEffect(() => {
@@ -11,50 +12,61 @@ const Projects = () => {
   }, []);
 
   return (
-<section className="projects" id="projects">
-  <div className="projects-container">
-    <div className="project-content">
-      <h2 className="project-header" data-aos="fade-up" data-aos-delay="300">
-        My <span> Projects </span>
-      </h2>
-    </div>
+    <section className="projects" id="projects">
+      <div className="projects-container">
+        <div className="project-content">
+          <h2 className="project-header" data-aos="fade-up" data-aos-delay="300">
+            My <span> Projects </span>
+          </h2>
+        </div>
 
-    <div className="card-container">
-      <div className="header-card">
-        <img src={kermapng} alt="kerma" />
-        <div className="card-text">Cafe & Bistro Kerma</div>
-        <div className="project-link">
-        <a href="https://www.tahkonkerma.fi/closed" target="_blank" rel="noopener noreferrer">View project</a>
+        <div className="card-container">
+          <div className="header-card">
+            <div className="image-container">
+              <img src={kermapng} alt="kerma" />
+              <div className="image-overlay"></div>
+            </div>
+            <div className="card-text">Cafe & Bistro Kerma</div>
+            <div className="project-link">
+              <a href="https://www.tahkonkerma.fi/closed" target="_blank" rel="noopener noreferrer">View project <FaArrowRight className="arrow-icon" /></a>
+            </div>
+          </div>
+
+          <div className="sub-card">
+            <div className="image-container">
+              <img src={kermapng} alt="sub1" />
+              <div className="image-overlay"></div>
+            </div>
+            <div className="card-text">Sub Card 1</div>
+            <div className="project-link">
+              <a href="https://www.tahkonkerma.fi/closed" target="_blank" rel="noopener noreferrer">View project <FaArrowRight className="arrow-icon" /></a>
+            </div>
+          </div>
+
+          <div className="sub-card">
+            <div className="image-container">
+              <img src={kermapng} alt="sub2" />
+              <div className="image-overlay"></div>
+            </div>
+            <div className="card-text">Sub Card 2</div>
+            <div className="project-link">
+              <a href="https://www.tahkonkerma.fi/closed" target="_blank" rel="noopener noreferrer">View project <FaArrowRight className="arrow-icon" /></a>
+            </div>
+          </div>
+
+          <div className="sub-card">
+            <div className="image-container">
+              <img src={kermapng} alt="sub3" />
+              <div className="image-overlay"></div>
+            </div>
+            <div className="card-text">Sub Card 3</div>
+            <div className="project-link">
+              <a href="https://www.tahkonkerma.fi/closed" target="_blank" rel="noopener noreferrer">View project <FaArrowRight className="arrow-icon" /></a>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="sub-card">
-        <img src={kermapng} alt="sub1" />
-        <div className="card-text">Sub Card 1</div>
-        <div className="project-link">
-        <a href="https://www.tahkonkerma.fi/closed" target="_blank" rel="noopener noreferrer">View project</a>
-        </div>
-      </div>
-
-      <div className="sub-card">
-        <img src={kermapng} alt="sub2" />
-        <div className="card-text">Sub Card 2</div>
-        <div className="project-link">
-        <a href="https://www.tahkonkerma.fi/closed" target="_blank" rel="noopener noreferrer">View project</a>
-        </div>
-      </div>
-
-      <div className="sub-card">
-        <img src={kermapng} alt="sub3" />
-        <div className="card-text">Sub Card 3</div>
-        <div className="project-link">
-        <a href="https://www.tahkonkerma.fi/closed" target="_blank" rel="noopener noreferrer">View project</a>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-
+    </section>
   );
 };
 
