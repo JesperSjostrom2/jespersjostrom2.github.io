1a7c300e60021bf446c7963db1c4859eda5af0b3
Jesper Sjöström <jespersjostrom2@gmail.com>
2024-07-02 13:12:14 +0200

    new short text

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -12,7 +12,7 @@ const Intro = () => {
       <div className="intro-content">
         <h1>Jesper Sjöström</h1>
         <h3>Front End Developer</h3>
-        <p>Passionate Frontend Developer with expertise in crafting visually appealing and user-friendly websites. Bringing creativity and technical expertise to deliver immersive web experiences.</p>
+        <p>From concept to code: Bringing digital visions to life with clean, responsive design and efficient front-end development.</p>
         <div className="intro-socials">
         <a href="https://www.linkedin.com/in/jesper-sj%C3%B6str%C3%B6m-521995232/" target="_blank" rel="noopener noreferrer"><FaLinkedin/></a>
         <a href="https://github.com/JesperSjostrom2?tab=overview&from=2023-04-01&to=2023-04-28" target="_blank" rel="noopener noreferrer"><FaGithub/></a>
