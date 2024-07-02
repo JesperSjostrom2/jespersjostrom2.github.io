ad9d5f90539f6097b1816c26d209eae065129019
Jesper Sjöström <jespersjostrom2@gmail.com>
2024-07-02 13:12:51 +0200

    updated about text & title

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -21,11 +21,20 @@ const About = () => {
             </div>
           </div>
         <div className="about-content">
-        <h2 className="heading" data-aos="fade-up" data-aos-delay="100"> About <span> Me</span></h2>
+        <h2 className="heading" data-aos="fade-up" data-aos-delay="100"> My <span> Journey </span></h2>
         <h3 data-aos="fade-up" data-aos-delay="150"> Jesper Sjöström - 24 y/o - Stockholm / Helsinki </h3>
-        <p data-aos="fade-up" data-aos-delay="200">I'm a passionate <span>Frontend Developer</span> with a keen interest in web design and user experience. I love creating visually appealing and user-friendly websites that provide a seamless browsing experience.
-  I have a strong background in <span>Web Development</span>, having completed my studies in Informatics and Media Technology at Huddinge Gymnasiet and later pursued a specialization in Web Development at Östra Gymnasiet. I have also honed my skills further through professional training at KYH Yrkeshögskola, where I focused on <span>Frontend Development.</span></p>
-        <p data-aos="fade-up" data-aos-delay="300">Throughout my <span>Journey</span>, I have gained hands-on experience working with various technologies, including HTML, CSS, JavaScript, and popular frontend frameworks like React. I am constantly expanding my knowledge and staying up-to-date with the latest industry trends to deliver modern and cutting-edge solutions.</p>
+        <p data-aos="fade-up" data-aos-delay="200">
+          My journey in <span>web development</span> has been a progression of discovery:
+          <ul><li>At Huddinge Gymnasiet, I studied Informatics and Media Technology for 3 years, sparking my interest in programming.
+       </li>
+        <li>A 4th technical year at <span>Östra Gymnasiet</span> where we did mostly programming helped me discover my passion for front-end development instead of backend.
+        </li>
+        <li>I then graduated as a Frontend Developer from <span>KYH Yrkeshögskola's</span> two-year program.
+        </li>
+        </ul>
+        My practical experience includes an internship at <span>Vello,</span> where I worked as a developer in a professional setting, and a comprehensive branding project for <span>Cafe & Bistro Kerma,</span> where I designed their logo and developed their website from scratch.
+        These experiences have reinforced my passion for creating clean, functional, and visually appealing web solutions.
+        </p>
         <a href="#contact" className="btn-cta" data-aos="fade-up" data-aos-delay="350">Say Hello!</a>
         </div>
       </div>
